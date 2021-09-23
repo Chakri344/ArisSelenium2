418311784c740f693567b4fe5f1f4b27e2b308b6
00005300 <00005300@AGI-LT-1977.arisglobal.com>
2021-09-23 16:05:25 +0530

    first commit

---
+++ src/main/java/com/chakri/Testing/App.java
@@ -0,0 +1,13 @@
+package com.chakri.Testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
