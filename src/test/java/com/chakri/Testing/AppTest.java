a2b5e6eb87c530c35add6ba42063f003de59f707
00005300 <00005300@AGI-LT-1977.arisglobal.com>
2021-09-23 16:09:27 +0530

    second commit

--- src/test/java/com/chakri/Testing/AppTest.java
+++ src/test/java/com/chakri/Testing/AppTest.java
@@ -1,38 +1,60 @@
-package com.chakri.Testing;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.Scanner;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+	
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public static void main(String[] args) throws InterruptedException  
+	{
+		
+	
+    System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+    WebDriver myD=new ChromeDriver();
+	
+	myD.get("https://www.amazon.in/");
+	myD.manage().window().maximize();
+	Thread.sleep(2000);
+	Select monthSelect=new Select(myD.findElement(By.id("searchDropdownBox")));
+	monthSelect.selectByVisibleText("Deals");
+	Thread.sleep(2000);
+	myD.findElement(By.xpath("//input[@type='text']")).sendKeys("Samsung zfold3");
+	myD.findElement(By.id("nav-search-submit-button")).click();
+	myD.findElement(By.xpath("//i[@class='a-icon a-icon-checkbox']")).click();
+	String vSearch= myD.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']/span[3]")).getText();
+	System.out.println(vSearch);
+	if(vSearch.contains("fold3"))
+	{
+		System.out.println("Pass");
+	}
+	else
+	{
+		System.out.println("Fail");
+	}
+	String vSearch1=myD.getTitle();
+	System.out.println(vSearch1);
+	if(vSearch1.contains("fold3"))
+	{
+		System.out.println("Pass");
+	}
+	else
+	{
+		System.out.println("Fail");
+	}
+	System.out.println("End of Test");
+	Thread.sleep(1000);
+	myD.close();
+		
+	}
+	
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
+
+
+
